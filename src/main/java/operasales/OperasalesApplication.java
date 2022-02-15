e0721bd2cea613b29ab88c6a38e283ced02e577c
Pavel <76093403+pavelkrasulin@users.noreply.github.com>
2022-02-15 21:35:54 +0400

    Добавляет исходные файлы для Spring Boot приложения

---
+++ src/main/java/operasales/OperasalesApplication.java
@@ -0,0 +1,13 @@
+package operasales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperasalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperasalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/operasales/OperasalesApplicationTests.java
@@ -0,0 +1,13 @@
+package operasales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperasalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
